6d1d5a183296f0e3af5fb6406ef4df23ac457470
Marek Krumal <krumalmarek@gmail.com>
2024-08-18 16:03:27 +0200

    MongoDB Setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 //const express = require("express"); zastaraly zapis
 
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Server is ready");
+    
+    console.log(process.env.MONGO_URI)
 })
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000")
+
 });
+
+//djbmv0Bds2r80HIj
