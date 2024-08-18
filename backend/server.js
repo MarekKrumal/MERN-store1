bcf2cbbb7322910e99f3bdee6e43c182ba896cad
Marek Krumal <krumalmarek@gmail.com>
2024-08-18 15:22:58 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+//const express = require("express"); zastaraly zapis
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Server is ready");
+})
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000")
+});
